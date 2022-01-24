85be79234b7505548459df1a895a6bcfee4f4c28
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-01-24 19:13:01 +0530

    use shorter test file

--- main.ml
+++ main.ml
@@ -14,10 +14,49 @@ let _ =
       Format.printf "identifier : %s\n" (Basic.string_of_ident id))
      envsig;
 
-  let inp = Parser.input_from_file "leibniz.dk" in
-  let leibniz : Entry.entry = List.hd @@ Parser.parse inp in
-  let ty = match leibniz with
+  let inp = Parser.input_from_file "testfile.dk" in
+  let testdecl : Entry.entry = List.hd @@ Parser.parse inp in
+  let ty = match testdecl with
   | Entry.Decl (_,_,_,_,t) -> t | _ -> failwith "unexpected" in
 
   (* Throws error (can't find cts.Term in signature) *)
   Format.printf "%a\n" Term.pp_term (norm ty) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  (* let rec gen : Term.term -> Term.term = function
+    | Const (l, name) ->
+      let open Basic in
+      let newident = 
+        let ident = string_of_ident (id name) in
+        if ident = "box" then 
+          mk_name (mk_mident "?") (mk_ident "?") 
+        else name 
+      in
+      Const (l,newident)
+    | App (f, a1, alist) ->
+        App (gen f, gen a1, List.map gen alist)
+    | Lam (l, id, ty, te) ->
+        Lam(l, id, Option.map gen ty, gen te)
+    | Pi (l, id, ty, te) ->
+        Pi (l, id, gen ty, gen te)
+    | x -> x
+  in *)
+  (* let leibniz : Entry.entry = match leibniz with
+  | Entry.Decl (l,id,scp,st,t) -> Entry.Decl (l,id,scp,st,gen t)
+  | _ -> failwith "not expected (1)"
+  in *)
