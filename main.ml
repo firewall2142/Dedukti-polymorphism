15ed814cd3c2e5ad7d54664418856f7fe596bae6
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-01-24 18:59:32 +0530

    initial commit

---
+++ main.ml
@@ -0,0 +1,23 @@
+open Parsers
+open Kernel
+open Api
+
+let _ =
+  let ctsinp = Parser.input_from_file "cts.dk" in
+  let env = Api.Env.init ctsinp in
+  let norm = Env.reduction env in
+  let envsig = Env.get_signature env in
+
+  (* Doesn't print anything *)
+  Signature.iter_symbols 
+    (fun _ id _ ->
+      Format.printf "identifier : %s\n" (Basic.string_of_ident id))
+     envsig;
+
+  let inp = Parser.input_from_file "leibniz.dk" in
+  let leibniz : Entry.entry = List.hd @@ Parser.parse inp in
+  let ty = match leibniz with
+  | Entry.Decl (_,_,_,_,t) -> t | _ -> failwith "unexpected" in
+
+  (* Throws error (can't find cts.Term in signature) *)
+  Format.printf "%a\n" Term.pp_term (norm ty) 
