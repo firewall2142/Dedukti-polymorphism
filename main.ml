148b2e3aab9a6a5a9cd7f0a7dd2a12250d6cf046
Ashish Barnawal <ashbrnwl.2000@gmail.com>
2022-02-11 13:00:55 +0530

    better working example

--- main.ml
+++ main.ml
@@ -6,12 +6,10 @@ module B = Kernel.Basic
 
 let files = ["cts.dk"]
 
-module Hterm : Hashtbl.HashedType = struct
+module D = Dsu.Make (struct
   open T
   type t = term
-
   let equal = term_eq
-
   (* consistent with equal *)
   let rec hash = fun t ->
   let comb a b = Hashtbl.hash (a, b) in
@@ -25,22 +23,61 @@ module Hterm : Hashtbl.HashedType = struct
         List.map hash (f :: t1 :: ts)
   | Lam (_,_,_,b) -> hash b
   | Pi (_,_,a,b) -> comb (hash a) (hash b)
-end
+end)
 
+
+
+let gen_term dsu te =
+  let rec aux t = let open T in
+    match t with
+    | Kind | Type _ | DB _ -> t
+    | Const _ -> if C.(is_var t) then D.repr dsu t else t
+    | App (f, t1, ts) ->
+        T.mk_App (aux f) (aux t1) (List.map aux ts)
+    | Lam (l,id,ty_opt,te) ->
+        let ty' = Option.bind ty_opt (fun x->Some (aux x)) in
+        T.mk_Lam l id ty' (aux te)
+    | Pi (l,id,a,b) -> T.mk_Pi l id (aux a) (aux b)
+  in
+  aux te
 
 
 let test env =
   let entry = List.hd @@ Parser.(parse @@ input_from_file "testfile.dk") in
   C.global_cstr := {cstrs=[]};
+  let dsu = D.empty () in
   match entry with
   | Entry.Decl (_,_,_,_,t) ->
     begin
       let sg = Env.get_signature env in
       C.Typing.check sg [] t (T.mk_Type B.dloc);
       let cstrs = (!C.global_cstr).cstrs in
-      List.iter (fun (l, r) ->
-        Format.printf "%a ~~~ %a\n\n" T.pp_term l T.pp_term r)
-        cstrs
+      let unite (l,r) =
+        Format.printf "%a ~~~ %a\n\n" T.pp_term l T.pp_term r;
+        D.unite dsu l r
+      in
+      List.iter unite cstrs;
+
+      (* Map containing [(v_repr, term)...] *)
+      let _ (*te_map*) =
+        let tbl = Hashtbl.create 100 in
+        let add2tbl (l, r) =
+          let add v te = 
+            let v = D.repr dsu v in
+            match Hashtbl.find_opt tbl v with
+            | None -> Hashtbl.replace tbl v te
+            | Some te' -> assert (T.term_eq te' te)
+          in
+          if (C.is_var l) && (C.is_var r) then ()
+          else if (C.is_var l) then add l r
+          else if (C.is_var r) then add r l
+          else assert (T.term_eq l r)
+        in
+        let _ = List.iter add2tbl cstrs in
+        tbl
+      in
+      let new_te = gen_term dsu t in
+      Format.printf "\nNew Term :\n%a\n" T.pp_term new_te
     end
   | _ -> failwith "unexpected entry"
 
